e1dea54d9e3bbdd9ad0047c790d2e46e4ef2961c
slan012 <e.larroumets@hotmail.fr>
2020-04-28 17:22:25 +0200

    Create pendu.py

---
+++ pendu.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+
+
+# début de partie le joueur donne son nom
+# si aucun score enregistré -> score = 0 pts => {'nom_joueur' : 0}
+mot ="test"
+
+
+	
+
+# mot de 8 lettres max choisi au hasard dans une liste
+# joueur saisi une lettre par tour (vérifier que c'est bien le cas (len(mot))
+
+# si lettre est dans le mot 
+	# afficher la (ou les) lettre(s) dans le mot à la bonne place
+	# mettre etoiles à la place des autres lettres
+	
+# 8 coups maximum -> sinon partie perdu
+# si mot trouvé -> score += nbre de coups restants
