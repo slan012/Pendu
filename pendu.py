8d4d54f4e777ea72433f02c26ba0c60904031715
slan012 <e.larroumets@hotmail.fr>
2020-04-28 23:53:10 +0200

    Update pendu.py
    Ajout de la fonction permettant de lire le fichier texte contenant la liste des mots, de transformer en liste et de choisir un mot random dans cette liste

--- pendu.py
+++ pendu.py
@@ -1,22 +1,48 @@
 # -*- coding: utf-8 -*-
 
+from random import randrange
 
 # début de partie le joueur donne son nom
 # si aucun score enregistré -> score = 0 pts => {'nom_joueur' : 0}
-mot ="test"
-lettre = 'a'
-mot = list(mot)
-print(mot)
+
+# ouvre le fichier 'liste_mots.txt' contenant les mots à trouver, lis le fichier et transforme la chaîne de mots en liste
+with open('liste_mots.txt','r') as fichier_txt :
+	liste_mots = fichier_txt.read()
+	liste_mots = liste_mots.split("\n")
+	
+
+# choisis un mot au hasard (random) dans la liste (effectue un random.randrange sur l'index de liste_mots en respectant la longueur de la liste)
 
 
+mot_choisi = liste_mots[randrange(0, len(liste_mots)-1)]
+
+	
 # Vérifie si une 'lettre' est dans le 'mot' et affiche le mot avec des étoiles sur les lettres qui n'y sont pas
+# def verifier_lettre(lettre, mot) 
 
-def verifier_lettre(lettre, mot) 
-	for i, element in enumerate(mot) :
-		if element != lettre :
-			mot[i] = '*'
-	mot = "".join(mot)
-	print("[ {} ]".format(mot))
+print(mot_choisi)
+mot_choisi = list(mot_choisi)
+mot_cache = list(mot_choisi)
+nbre_coups = 8
+
+for i, element in enumerate(mot_cache) :
+		mot_cache[i] = '*'
+
+while nbre_coups > 0 :
+	mot_choisi = list(mot_choisi)
+	lettre = input("Lettre : ")
+	for i, element in enumerate(mot_choisi) :
+		if element == lettre.upper() :
+			mot_cache[i] = lettre.upper()
+	print(" ".join(mot_cache))
+	nbre_coups -= 1
+	
+if nbre_coups == 0 :
+	print("Nombre de coups max atteint")
+
+
+	
+
 	
 
 
